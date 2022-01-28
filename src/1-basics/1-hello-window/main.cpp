23e293e4d73320186be02713ad4767b47bf333f3
Alp Cihan <alp.cihan@trendyol.com>
2022-01-28 23:44:39 +0300

    Create window

--- src/1-basics/1-hello-window/main.cpp
+++ src/1-basics/1-hello-window/main.cpp
@@ -2,6 +2,45 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
-    return 0;   
+int main() 
+{
+    const bool isGlfwInitialized = glfwInit();
+
+    if (!isGlfwInitialized) 
+    {
+        std::cout << "Failed to initialize GLFW!" << std::endl;
+        return 0;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "hello-window", NULL, NULL);
+
+    if (!window) 
+    {
+        glfwTerminate();
+        return 0;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+    {
+        std::cout << "Failed to initialize GLAD!" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) 
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
 }
