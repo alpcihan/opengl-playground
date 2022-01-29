a3a9896ef1e7278542b958100ce91d399a8b3d57
Alp Cihan <alp.cihan@trendyol.com>
2022-01-29 19:48:29 +0300

    Create triangle

--- src/1-basics/2-hello-triangle/main.cpp
+++ src/1-basics/2-hello-triangle/main.cpp
@@ -5,10 +5,26 @@ int main()
 {
     Window window = Window();
 
-    while(!window.isClosed()) {
+    const float positions[18] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    glEnableVertexAttribArray(0);
+
+    while (!window.isClosed())
+    {
         window.clear();
 
         /* Render comes here. */
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         window.update();
     }
