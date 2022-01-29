d70d01c92c5855c20ea02629fb3d64fa597d616a
alpcihan <alpcihan.ac@gmail.com>
2022-01-29 20:34:29 +0300

    Init 3-shader project

---
+++ src/1-basics/3-shader/Window.cpp
@@ -0,0 +1,68 @@
+#include "Window.h"
+#include "spdlog/spdlog.h"
+
+static unsigned windowCount = 0;
+
+Window::Window(const WindowProps &windowProps)
+    : windowProps(windowProps)
+{
+    this->initialize();
+}
+
+Window::~Window()
+{
+    this->shutdown();
+}
+
+void Window::update()
+{
+    glfwMakeContextCurrent(this->window);
+    glfwSwapBuffers(this->window);
+    glfwPollEvents();
+}
+
+void Window::clear()
+{
+    glfwMakeContextCurrent(this->window);
+    OpenGLContext::clear();
+}
+
+bool Window::isClosed()
+{
+    return glfwWindowShouldClose(this->window);
+}
+
+void Window::initialize()
+{
+    if (windowCount == 0)
+    {
+        bool success = glfwInit();
+        if (success)
+            spdlog::error("Could not initialize GLFW!");
+    }
+
+    this->window = glfwCreateWindow(
+        this->windowProps.width,
+        this->windowProps.height,
+        this->windowProps.title.c_str(),
+        NULL,
+        NULL);
+
+    if (!window)
+        spdlog::error("Could not initialize window!");
+    else
+        ++windowCount;
+
+    glfwMakeContextCurrent(this->window);
+
+    OpenGLContext::init();
+}
+
+void Window::shutdown()
+{
+    glfwDestroyWindow(this->window);
+    --windowCount;
+
+    if (windowCount == 0)
+        glfwTerminate();
+}
---
+++ src/1-basics/3-shader/Window.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+#include "OpenGLContext.h"
+
+struct WindowProps
+{
+    unsigned width;
+    unsigned height;
+    std::string title;
+
+    WindowProps(unsigned width = 1600,
+                unsigned height = 900,
+                const std::string &title = "My Project")
+        : title(title), width(width), height(height)
+    {
+    }
+};
+
+class Window
+{
+public:
+    WindowProps windowProps;
+    GLFWwindow *window;
+
+public:
+    Window(const WindowProps &windowProps = WindowProps());
+    ~Window();
+
+    void update();
+    void clear();
+
+    bool isClosed();
+
+private:
+    void initialize();
+    void shutdown();
+};
