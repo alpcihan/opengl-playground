8e04c22875b329f56125fd98742eaab0939da615
alpcihan <alpcihan.ac@gmail.com>
2022-04-22 21:31:20 +0200

    Add normal vector attrib to the Vertex struct

--- src/core/model/Vertex.h
+++ src/core/model/Vertex.h
@@ -4,10 +4,19 @@
 
 struct Vertex
 {
-    Vertex(float x, float y, float z, float u, float v)
-        : position(glm::vec3(x,y,z)), texCoords(glm::vec2(u,v))
+public:
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texCoords;
+
+public:
+    Vertex() = default;
+
+    Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 texCoords)
+        : position(position), normal(normal), texCoords(texCoords)
     {}
 
-    glm::vec3 position;
-    glm::vec2 texCoords;
+    Vertex(float x, float y, float z, float nx, float ny, float nz, float u, float v)
+        : position(glm::vec3(x, y, z)), normal(glm::vec3(nx, ny, nz)) ,texCoords(glm::vec2(u, v))
+    {}
 };
